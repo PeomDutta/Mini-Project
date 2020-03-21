893ae744a9fb79b3dfa7cbcbdac9dc707a93662b
S,S,Saruar Jahan <40640156+sjalim@users.noreply.github.com>
2020-03-21 23:43:26 +0600

    frist one

---
+++ app/src/main/java/com/example/miniproject/IEDCRCONTACT.java
@@ -0,0 +1,20 @@
+package com.example.miniproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class IEDCRCONTACT extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_iedcrcontact);
+
+
+    }
+}
